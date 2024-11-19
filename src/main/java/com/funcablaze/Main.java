09514a83d773a3ac0894a2aa6700e61b9220a0fd
Song_Mojing <Song_Mojing@outlook.com>
2024-11-19 20:02:54 +0800

    正常更改

--- src/main/java/com/funcablaze/Main.java
+++ src/main/java/com/funcablaze/Main.java
@@ -1,21 +1,92 @@
 package com.funcablaze;
 
+import com.funcablaze.frame.ChatWindow;
 import com.funcablaze.frame.Frame;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+	
+	public static final String title = "E.communication";
+	private static Frame main;
+	public static Map<String, ChatWindow> chatWindow = new HashMap<>();
+	private static TrayIcon trayIcon;
+	private static boolean allowNotifications = false;
+	
+	public static void main(String[] args) {
+		SystemTrayInitial();
+		main = new Frame(300, 700, 0, 0);
+		showMainWindow();
 
-    private static final String title = "E.communication";
-    private static Frame main;
+//        showTrayMessage(title, "弹出消息", TrayIcon.MessageType.INFO);
+	}
+	
+	/**
+	 * 显示主窗口
+	 */
+	public static void showMainWindow() {
+		if (main != null) {
+			main.setVisible(true);
+			main.setAlwaysOnTop(true);
+			main.toFront();
+		}
+	}
+	
+	/**
+	 * 弹出信息
+	 */
+	public static void showTrayMessage(String title, String message, TrayIcon.MessageType messageType) {
+		if (trayIcon != null && allowNotifications) {
+			trayIcon.displayMessage(title, message, messageType);
+		}
+	}
+	
+	/**
+	 * 系统托盘
+	 */
+	private static void SystemTrayInitial() {
+		//判断系统是否支持托盘
+		if (!SystemTray.isSupported()) {
+			return;
+		}
+		try {
+			SystemTray systemTray = SystemTray.getSystemTray();//获取系统默认托盘
+			Image image = Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/img/icon.png"));//系统栏图标
+			trayIcon = new TrayIcon(image, title, createMenu());//添加图标,标题,内容,菜单
+			trayIcon.setImageAutoSize(true);//设置图像自适应
+			trayIcon.addActionListener(new SysActionListener());//双击打开窗口
+			systemTray.add(trayIcon);//添加托盘
+		} catch (AWTException ignored) {
+		}
+	}
+	
+	//托盘中的菜单
+	private static PopupMenu createMenu() {
+		PopupMenu menu = new PopupMenu();//创建弹出式菜单
+		MenuItem exitItem = new MenuItem("exit EC");//创建菜单项
+		//给菜单项添加事件监听器，单击时退出系统
+		exitItem.addActionListener(e -> System.exit(0));
+		CheckboxMenuItem notificationsItem = new CheckboxMenuItem("allow Notifications");//创建菜单项
+		// 可勾选的ItemItem
+		notificationsItem.setState(allowNotifications);
+		notificationsItem.addActionListener(e -> allowNotifications = !allowNotifications);
+		//给菜单项添加事件监听器，单击时退出系统
+		exitItem.addActionListener(e -> System.exit(0));
+		menu.add(notificationsItem);//添加打开系统菜单
+		menu.addSeparator();//菜单分割符
+		menu.add(exitItem);//添加退出系统菜单
+		return menu;
+	}
 
-    public static void main(String[] args) {
-        main = new Frame(300, 700, -10, 0);
-        showMainWindow();
-    }
-
-    public static void showMainWindow() {
-        if (main != null) {
-            main.setVisible(true);
-            main.setAlwaysOnTop(true);
-        }
-    }
+	//双击托盘弹出主窗体
+	static class SysActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			showMainWindow();
+		}
+	}
 }
--- src/main/java/com/funcablaze/frame/Frame.java
+++ src/main/java/com/funcablaze/frame/Frame.java
@@ -1,51 +1,168 @@
 package com.funcablaze.frame;
 
+import com.funcablaze.Main;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Objects;
 
 public class Frame extends JFrame {
-
-    public Frame(int width, int height, int x, int y) {
-        // 设置 JFrame 无边框
-        setUndecorated(true);
-        setSize(width, height);
-        setLocationRelativeTo(null);
-        setLocation(x, y);
-        JPanel main = addTop();
-    }
-
-    private JPanel addTop() {
-        JPanel back = new JPanel();
-        back.setBackground(new Color(30, 31, 34));
-        // 自定义标题栏
-        JPanel titleBar = new JPanel();
-        {
-            titleBar.setBackground(new Color(0, 0, 0, 0));
-            titleBar.setPreferredSize(new Dimension(getWidth(), 25));
-            titleBar.setLayout(new BorderLayout());
-        }
-        // 关闭按钮
-        JLabel closeButton = new JLabel("X");
-        {
-            closeButton.setFont(new Font("微软雅黑", Font.BOLD, 12));
-            closeButton.setPreferredSize(new Dimension(40, 30));
-            closeButton.setHorizontalAlignment(SwingConstants.CENTER);
-            closeButton.setForeground(Color.WHITE);
-            closeButton.setBackground(new Color(255, 0, 0, 255));
-//            closeButton.setBackground(new Color(0, 0, 0, 0));
-            closeButton.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    setVisible(false);
-                }
-            });
-            titleBar.add(closeButton, BorderLayout.EAST);
-        }
-        // 添加自定义标题栏和主面板
-        back.add(titleBar, BorderLayout.WEST);
-        add(back, BorderLayout.CENTER);
-        return back;
-    }
+	
+	private final JList<User> userList;
+	
+	public Frame(int width, int height, int x, int y) {
+		// 设置 JFrame 无边框
+		setTitle(Main.title);
+		setUndecorated(true);
+		setSize(width, height);
+		setLocation(x, y);
+		addTop();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+				 UnsupportedLookAndFeelException ignored) {
+		}
+		userList = createList();
+		add(userList);
+	}
+	
+	private JList<User> createList() {
+		// create List 模型
+		DefaultListModel<User> model = new DefaultListModel<>();
+		// 将项添加到模型
+		model.addElement(new User("张三", "一二三四五", "192.168.12.100", null, false, true, 5));
+		model.addElement(new User("李四", "上山打老虎", "192.168.12.101", null, false, false, -3));
+		// 使用 model 创建 JList
+		JList<User> list = new JList<>(model);
+		// 设置单元格渲染器
+		list.setCellRenderer(new UserRenderer());
+		// 双击监听
+		list.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					User user = list.getSelectedValue();
+					if (user != null) {
+						if (Main.chatWindow.containsKey(user.ip)) {
+							Main.chatWindow.get(user.ip).setVisible(true);
+						} else {
+							Main.chatWindow.put(user.ip, new ChatWindow(user.name(), user.ip));
+						}
+					}
+				}
+			}
+		});
+		return list;
+	}
+	
+	public record User(String name, String message, String ip, ImageIcon icon, boolean isOnline, boolean isTop,
+					   int letter) {
+	}
+	
+	public static class UserRenderer extends JPanel implements ListCellRenderer<User> {
+		
+		private JLabel Icon = new JLabel(), Name = new JLabel(), Info = new JLabel(), letter = new JLabel();
+		
+		public UserRenderer() {
+			setLayout(new BorderLayout(5, 5));
+			
+			JPanel panelText = new JPanel(new GridLayout(0, 1));
+			panelText.add(Name);
+			panelText.add(Info);
+			add(Icon, BorderLayout.WEST);
+			add(panelText, BorderLayout.CENTER);
+			add(letter, BorderLayout.EAST);
+		}
+		
+		@Override
+		public Component getListCellRendererComponent(JList<? extends User> list, User user, int index, boolean isSelected, boolean cellHasFocus) {
+			ImageIcon icon = new ImageIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/Users/icon/" + user.ip))).getImage().getScaledInstance(40, 40, Image.SCALE_DEFAULT));
+			Icon.setIcon(icon);
+			Name.setText(user.name());
+			Font NameFont = new Font("微软雅黑", Font.BOLD, 14);
+			Name.setFont(NameFont);
+			Info.setText(user.message);
+			Info.setForeground(Color.GRAY);
+			letter.setForeground(Color.RED);
+			letter.setPreferredSize(new Dimension(40, 50));
+			letter.setOpaque(true);
+			letter.setHorizontalAlignment(SwingConstants.CENTER);
+			Font LetterFont = new Font("consolas", Font.PLAIN, 15);
+			letter.setFont(LetterFont);
+			if (user.letter > 0) {
+				letter.setText(user.letter + "");
+			} else if (user.letter == 0) {
+				letter.setText("");
+			} else {
+				letter.setText("•" + Math.abs(user.letter));
+				letter.setForeground(Color.DARK_GRAY);
+			}
+			// 设置 Opaque 以更改 JLabel 的背景颜色
+			Name.setOpaque(true);
+			Info.setOpaque(true);
+			Icon.setOpaque(true);
+			// 约束大小
+			setPreferredSize(new Dimension(getWidth(), 50));
+			
+			if (isSelected) {
+				// 被选中
+				Name.setBackground(selectColor);
+				Info.setBackground(selectColor);
+				Icon.setBackground(selectColor);
+				letter.setBackground(selectColor);
+				setBackground(selectColor);
+			} else {
+				Name.setBackground(list.getBackground());
+				Info.setBackground(list.getBackground());
+				Icon.setBackground(list.getBackground());
+				letter.setBackground(list.getBackground());
+				setBackground(list.getBackground());
+			}
+			return this;
+		}
+	}
+	
+	private void addTop() {
+		// 自定义标题栏
+		JPanel titleBar = new JPanel();
+		titleBar.setLayout(new BorderLayout());
+		titleBar.setBackground(backgroundColor);
+		titleBar.setPreferredSize(new Dimension(getWidth(), 30));
+		
+		// 关闭按钮
+		JButton closeButton = new JButton("X");
+		Font LetterFont = new Font("微软雅黑", Font.PLAIN, 15);
+		closeButton.setFont(LetterFont);
+		closeButton.setFocusPainted(false);
+		closeButton.setBorderPainted(false);
+		closeButton.setForeground(Color.WHITE);
+		closeButton.setBackground(backgroundColor);
+		closeButton.setHorizontalAlignment(SwingConstants.CENTER);
+		closeButton.setPreferredSize(new Dimension(45, 30));
+		closeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				closeButton.setBackground(exitfocusColor);
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				closeButton.setBackground(backgroundColor);
+			}
+		});
+		titleBar.add(closeButton, BorderLayout.EAST);
+		add(titleBar, BorderLayout.NORTH);
+	}
+	
+	private static final Color
+			backgroundColor = new Color(30, 31, 34),
+			exitfocusColor = new Color(255, 0, 0),
+			selectColor = new Color(201, 196, 196);
 }
